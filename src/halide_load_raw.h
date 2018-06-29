122d27903a81586f6ee9d1cd507cec7e509bb8ac
JanzenLiu <chengtsung_liu@yahoo.com>
2018-06-29 18:39:33 +0800

    Fix bugs (error when I build locally)

--- src/halide_load_raw.h
+++ src/halide_load_raw.h
@@ -1,11 +1,16 @@
 // basic raw image loader for Halide::Buffer<T> or other image with same API
 // code largely appropriated from halide_image_io.h
 
+// Fix@Jun-29 by Janzen:
+//  1. Replace directly passed format and arguments to by a char* with value copied
+
 #ifndef HALIDE_LOAD_RAW_H
 #define HALIDE_LOAD_RAW_H
 
 #include "Halide.h"
 #include "halide_image_io.h"
+#include <stdio.h>
+
 
 namespace Halide {
 namespace Tools {
@@ -45,9 +50,12 @@ inline void swap_endian_16(uint16_t &value) {
 
 template<Internal::CheckFunc check = Internal::CheckFail>
 bool load_raw(const std::string &filename, uint16_t* data, int width, int height) {
+    char out [1000];  // to store output
+    int ret;  // to store string formatting result
 
     Internal::PipeOpener f(("../tools/dcraw -c -D -6 -W -g 1 1 " + filename).c_str(), "r");
-    if (!check(f.f != nullptr, "File %s could not be opened for reading\n", filename.c_str())) return false;
+    ret = sprintf(out, "File %s could not be opened for reading\n", filename.c_str());
+    if (!check(f.f != nullptr, out)) return false;
 
     int in_width, in_height, maxval;
     char header[256];
@@ -63,9 +71,11 @@ bool load_raw(const std::string &filename, uint16_t* data, int width, int height
     f.readLine(buf, 1024);
     if (!check(sscanf(buf, "%d %d\n", &in_width, &in_height) == 2, "Could not read PGM width and height\n")) return false;
 
-    if (!check(in_width == width, "Input image '%s' has width %d, but must must have width of %d\n", filename.c_str(), in_width, width)) return false;
+    ret = sprintf(out, "Input image '%s' has width %d, but must must have width of %d\n", filename.c_str(), in_width, width);
+    if (!check(in_width == width, out)) return false;
 
-    if (!check(in_height == height, "Input image '%s' has height %d, but must must have height of %d\n", filename.c_str(), in_height, height)) return false;
+    ret = sprintf(out, "Input image '%s' has height %d, but must must have height of %d\n", filename.c_str(), in_height, height);
+    if (!check(in_height == height, out)) return false;
 
     f.readLine(buf, 1024);
     if (!check(sscanf(buf, "%d", &maxval) == 1, "Could not read PGM max value\n")) return false;
--- src/merge.h
+++ src/merge.h
@@ -3,10 +3,13 @@
 
 #include "Halide.h"
 
+// Fix@Jun-29 by Janzen:
+//  1. Replace Halide::Image by Halide::Buffer
+
 /*
  * merge -- fully merges aligned frames in the temporal and spatial
  * dimension to produce one denoised bayer frame.
  */
-Halide::Func merge(Halide::Image<uint16_t> imgs, Halide::Func alignment);
+Halide::Func merge(Halide::Buffer<uint16_t> imgs, Halide::Func alignment);
 
 #endif
